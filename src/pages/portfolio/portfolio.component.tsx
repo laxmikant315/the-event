400163acb86e33afb0b1d882fff74aa9ae2b3a1c
laxmikantphadke <laxmikantphadke@gmail.com>
2021-09-12 12:03:58 +0530

    sd

--- src/pages/portfolio/portfolio.component.tsx
+++ src/pages/portfolio/portfolio.component.tsx
@@ -1,4 +1,14 @@
-import { Button, Card, Col, Progress, Row, Skeleton, Space, Spin } from "antd";
+import {
+  Button,
+  Card,
+  Col,
+  Modal,
+  Progress,
+  Row,
+  Skeleton,
+  Space,
+  Spin,
+} from "antd";
 import axios from "axios";
 import moment from "moment";
 import { useEffect, useState } from "react";
@@ -11,73 +21,88 @@ const serverUrl = process.env.REACT_APP_SERVER_URL;
 const StockCard = ({ data }: any) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   return (
-    <Col xs={24} sm={12} md={12} lg={8} xl={6} span={8}>
-      <Card
-        title={
-          <Row justify="space-between">
-            <span>
-              <Button
-                type="text"
-                size="small"
-                onClick={() => setIsModalVisible(true)}
-              >
-                <BarChartOutlined />
-              </Button>
-              {"   "}
-              {data.symbol} <small>{data.last_price}</small>
-            </span>
-            <span style={{ color: data["pnl"] > 0 ? "#73d13d" : "#ff4d4f" }}>
-              <Button
-                type="text"
-                size="large"
-                target="_blank"
-                href={`https://in.tradingview.com/chart/i6VwIssE/?symbol=NSE%3A${data.symbol}`}
-              >
-                <LineChartOutlined />
-              </Button>
-              {data.pnl.toFixed(2)}
-            </span>
-          </Row>
-        }
-        bordered={false}
-        size="small"
-        style={{ marginBottom: 20 }}
+    <>
+      <Modal
+        title="Technical"
+        footer=""
+        width={800}
+        bodyStyle={{ height: 400 }}
+        visible={isModalVisible}
+        onCancel={() => setIsModalVisible(false)}
       >
-        {data["pnl"] > 0 ? (
-          <Progress
-            percent={+data["progress"].toFixed(1)}
-            status="active"
-            strokeColor="#73d13d"
-          />
-        ) : (
-          <Progress
-            percent={+Math.abs(data["progress"]).toFixed(1)}
-            status="active"
-            strokeColor="#ff4d4f"
-          />
-        )}
+        <iframe
+          src={`https://mo.streak.tech/?utm_source=context-menu&utm_medium=kite&stock=NSE:${data.symbol}&theme=dark`}
+          style={{ height: "100%", width: "100%", border: 0 }}
+        />
+      </Modal>
+      <Col xs={24} sm={12} md={12} lg={8} xl={6} span={8}>
+        <Card
+          title={
+            <Row justify="space-between">
+              <span>
+                <Button
+                  type="text"
+                  size="small"
+                  onClick={() => setIsModalVisible(true)}
+                >
+                  <BarChartOutlined />
+                </Button>
+                {"   "}
+                {data.symbol} <small>{data.last_price}</small>
+              </span>
+              <span style={{ color: data["pnl"] > 0 ? "#73d13d" : "#ff4d4f" }}>
+                <Button
+                  type="text"
+                  size="large"
+                  target="_blank"
+                  href={`https://in.tradingview.com/chart/i6VwIssE/?symbol=NSE%3A${data.symbol}`}
+                >
+                  <LineChartOutlined />
+                </Button>
+                {data.pnl.toFixed(2)}
+              </span>
+            </Row>
+          }
+          bordered={false}
+          size="small"
+          style={{ marginBottom: 20 }}
+        >
+          {data["pnl"] > 0 ? (
+            <Progress
+              percent={+data["progress"].toFixed(1)}
+              status="active"
+              strokeColor="#73d13d"
+            />
+          ) : (
+            <Progress
+              percent={+Math.abs(data["progress"]).toFixed(1)}
+              status="active"
+              strokeColor="#ff4d4f"
+            />
+          )}
 
-        <Descriptions bordered size="small">
-          <Descriptions.Item label="Buy Date" span={4}>
-            {moment(data.buy_date).format("D MMM h:mm a")}
-          </Descriptions.Item>
-          <Descriptions.Item label="Buy Price" span={4}>
-            {data.buy_price}
-          </Descriptions.Item>
+          <Descriptions bordered size="small">
+            <Descriptions.Item label="Buy Date" span={4}>
+              {moment(data.buy_date).format("D MMM h:mm a")}
+            </Descriptions.Item>
+            <Descriptions.Item label="Buy Price" span={4}>
+              {data.buy_price}
+            </Descriptions.Item>
 
-          <Descriptions.Item label="Target" span={4}>
-            {data.target} ({data.targetPer}%)
-          </Descriptions.Item>
-          <Descriptions.Item label="Stoploss" span={4}>
-            {data.stopLoss} ({data.stopLossPer}%)
-          </Descriptions.Item>
-          <Descriptions.Item label="Value" span={4}>
-            x{data.quantity} = {(data.buy_price * data.quantity).toFixed(2)} &
-            Now {(data.last_price * data.quantity).toFixed(2)}
-          </Descriptions.Item>
-        </Descriptions>
-      </Card>
-    </Col>
+            <Descriptions.Item label="Target" span={4}>
+              {data.target} ({data.targetPer}%)
+            </Descriptions.Item>
+            <Descriptions.Item label="Stoploss" span={4}>
+              {data.stopLoss} ({data.stopLossPer}%)
+            </Descriptions.Item>
+            <Descriptions.Item label="Value" span={4}>
+              x{data.quantity} = {(data.buy_price * data.quantity).toFixed(2)} &
+              Now {(data.last_price * data.quantity).toFixed(2)}
+            </Descriptions.Item>
+          </Descriptions>
+        </Card>
+      </Col>
+    </>
   );
 };
 
