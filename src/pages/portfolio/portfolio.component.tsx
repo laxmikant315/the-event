6e3f276db974f297faf9f6ff4b329aed9c1c55b1
laxmikantphadke <laxmikantphadke@gmail.com>
2021-09-12 11:10:07 +0530

    asd

--- src/pages/portfolio/portfolio.component.tsx
+++ src/pages/portfolio/portfolio.component.tsx
@@ -4,71 +4,82 @@ import moment from "moment";
 import { useEffect, useState } from "react";
 import { Descriptions } from "antd";
 import Title from "antd/lib/typography/Title";
-
+import BarChartOutlined from "@ant-design/icons/lib/icons/BarChartOutlined";
 import { LineChartOutlined } from "@ant-design/icons";
 const serverUrl = process.env.REACT_APP_SERVER_URL;
 
-const StockCard = ({ data }: any) => (
-  <Col xs={24} sm={12} md={12} lg={8} xl={6} span={8}>
-    <Card
-      title={
-        <Row justify="space-between">
-          <span>
-            {data.symbol} <small>{data.last_price}</small>
-          </span>
-          <span style={{ color: data["pnl"] > 0 ? "#73d13d" : "#ff4d4f" }}>
-            <Button
-              type="text"
-              size="large"
-              target="_blank"
-              href={`https://in.tradingview.com/chart/i6VwIssE/?symbol=NSE%3A${data.symbol}`}
-            >
-              <LineChartOutlined />
-            </Button>
-            {data.pnl.toFixed(2)}
-          </span>
-        </Row>
-      }
-      bordered={false}
-      size="small"
-      style={{ marginBottom: 20 }}
-    >
-      {data["pnl"] > 0 ? (
-        <Progress
-          percent={+data["progress"].toFixed(1)}
-          status="active"
-          strokeColor="#73d13d"
-        />
-      ) : (
-        <Progress
-          percent={+Math.abs(data["progress"]).toFixed(1)}
-          status="active"
-          strokeColor="#ff4d4f"
-        />
-      )}
+const StockCard = ({ data }: any) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  return (
+    <Col xs={24} sm={12} md={12} lg={8} xl={6} span={8}>
+      <Card
+        title={
+          <Row justify="space-between">
+            <span>
+              <Button
+                type="text"
+                size="small"
+                onClick={() => setIsModalVisible(true)}
+              >
+                <BarChartOutlined />
+              </Button>
+              {"   "}
+              {data.symbol} <small>{data.last_price}</small>
+            </span>
+            <span style={{ color: data["pnl"] > 0 ? "#73d13d" : "#ff4d4f" }}>
+              <Button
+                type="text"
+                size="large"
+                target="_blank"
+                href={`https://in.tradingview.com/chart/i6VwIssE/?symbol=NSE%3A${data.symbol}`}
+              >
+                <LineChartOutlined />
+              </Button>
+              {data.pnl.toFixed(2)}
+            </span>
+          </Row>
+        }
+        bordered={false}
+        size="small"
+        style={{ marginBottom: 20 }}
+      >
+        {data["pnl"] > 0 ? (
+          <Progress
+            percent={+data["progress"].toFixed(1)}
+            status="active"
+            strokeColor="#73d13d"
+          />
+        ) : (
+          <Progress
+            percent={+Math.abs(data["progress"]).toFixed(1)}
+            status="active"
+            strokeColor="#ff4d4f"
+          />
+        )}
 
-      <Descriptions bordered size="small">
-        <Descriptions.Item label="Buy Date" span={4}>
-          {moment(data.buy_date).format("D MMM h:mm a")}
-        </Descriptions.Item>
-        <Descriptions.Item label="Buy Price" span={4}>
-          {data.buy_price}
-        </Descriptions.Item>
+        <Descriptions bordered size="small">
+          <Descriptions.Item label="Buy Date" span={4}>
+            {moment(data.buy_date).format("D MMM h:mm a")}
+          </Descriptions.Item>
+          <Descriptions.Item label="Buy Price" span={4}>
+            {data.buy_price}
+          </Descriptions.Item>
 
-        <Descriptions.Item label="Target" span={4}>
-          {data.target} ({data.targetPer}%)
-        </Descriptions.Item>
-        <Descriptions.Item label="Stoploss" span={4}>
-          {data.stopLoss} ({data.stopLossPer}%)
-        </Descriptions.Item>
-        <Descriptions.Item label="Value" span={4}>
-          x{data.quantity} = {(data.buy_price * data.quantity).toFixed(2)} & Now{" "}
-          {(data.last_price * data.quantity).toFixed(2)}
-        </Descriptions.Item>
-      </Descriptions>
-    </Card>
-  </Col>
-);
+          <Descriptions.Item label="Target" span={4}>
+            {data.target} ({data.targetPer}%)
+          </Descriptions.Item>
+          <Descriptions.Item label="Stoploss" span={4}>
+            {data.stopLoss} ({data.stopLossPer}%)
+          </Descriptions.Item>
+          <Descriptions.Item label="Value" span={4}>
+            x{data.quantity} = {(data.buy_price * data.quantity).toFixed(2)} &
+            Now {(data.last_price * data.quantity).toFixed(2)}
+          </Descriptions.Item>
+        </Descriptions>
+      </Card>
+    </Col>
+  );
+};
 
 const Alerts = () => {
   const [data, setData] = useState([]);
