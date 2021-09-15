455c5213b638f24d75bd71b181942f14d640fd67
laxmikantphadke <laxmikantphadke@gmail.com>
2021-09-15 10:57:44 +0530

    asd

--- src/pages/portfolio/portfolio.component.tsx
+++ src/pages/portfolio/portfolio.component.tsx
@@ -1,4 +1,5 @@
 import {
+  Badge,
   Button,
   Card,
   Col,
@@ -65,110 +66,121 @@ const StockCard = ({ data: dataFromProps }: any) => {
         />
       </Modal>
       <Col xs={24} sm={12} md={12} lg={12} xl={6} span={8}>
-        <Card
-          title={
-            <Row justify="space-between">
-              <span>
-                <Button
-                  loading={loading}
-                  type="text"
-                  size="small"
-                  onClick={() => setIsModalVisible(true)}
-                  style={{
-                    background: techColor,
-                  }}
-                >
-                  <BarChartOutlined /> {techIndex}
-                </Button>
-                {"   "}
-                {data.symbol} <small>{data.last_price}</small>
-              </span>
-              <span style={{ color: data["pnl"] > 0 ? "#73d13d" : "#ff4d4f" }}>
-                <Button
-                  type="text"
-                  size="large"
-                  target="_blank"
-                  href={`https://in.tradingview.com/chart/i6VwIssE/?symbol=NSE%3A${data.symbol}`}
-                >
-                  <LineChartOutlined />
-                </Button>
-                {data.pnl.toFixed(2)}
-
-                <Popconfirm
-                  placement="bottomRight"
-                  title={"Are you sure?"}
-                  onConfirm={async () => {
-                    const order = await axios.get(
-                      `${serverUrl}/sell/${data.symbol}`
-                    );
-
-                    if (order.data && order.data.status === "order_completed") {
-                      notification.success({
-                        message: "Order Success",
-                        description: "Order successfully placed.",
-                      });
-                    } else {
-                      notification.error({
-                        message: "Order Failed",
-                        description: "Order placing failed.",
-                      });
-                    }
-                  }}
-                  okText="Yes"
-                  cancelText="No"
-                  okButtonProps={{ size: "large" }}
-                  cancelButtonProps={{ size: "large" }}
-                >
-                  <Button type="text" size="small">
-                    Sell
-                  </Button>
-                </Popconfirm>
-              </span>
-            </Row>
-          }
-          bordered={false}
-          size="small"
-          style={{ marginBottom: 20 }}
+        <Badge
+          status="warning"
+          count="P"
+          style={{ display: data.isInPositions ? "block" : "none" }}
         >
-          {data["pnl"] > 0 ? (
-            <Progress
-              percent={+data["progress"].toFixed(1)}
-              status="active"
-              strokeColor="#73d13d"
-            />
-          ) : (
-            <Progress
-              percent={+Math.abs(data["progress"]).toFixed(1)}
-              status="active"
-              strokeColor="#ff4d4f"
-            />
-          )}
+          <Card
+            title={
+              <Row justify="space-between">
+                <span>
+                  <Button
+                    loading={loading}
+                    type="text"
+                    size="small"
+                    onClick={() => setIsModalVisible(true)}
+                    style={{
+                      background: techColor,
+                    }}
+                  >
+                    <BarChartOutlined /> {techIndex}
+                  </Button>
+                  {"   "}
+                  {data.symbol} <small>{data.last_price}</small>
+                </span>
+                <span
+                  style={{ color: data["pnl"] > 0 ? "#73d13d" : "#ff4d4f" }}
+                >
+                  <Button
+                    type="text"
+                    size="large"
+                    target="_blank"
+                    href={`https://in.tradingview.com/chart/i6VwIssE/?symbol=NSE%3A${data.symbol}`}
+                  >
+                    <LineChartOutlined />
+                  </Button>
+                  {data.pnl.toFixed(2)}
 
-          <Descriptions bordered size="small">
-            <Descriptions.Item label="Buy Date" span={4}>
-              {moment(data.buy_date).format("D MMM h:mm a")}
-            </Descriptions.Item>
-            <Descriptions.Item label="Buy Price" span={4}>
-              {data.buy_price}
-            </Descriptions.Item>
+                  <Popconfirm
+                    placement="bottomRight"
+                    title={"Are you sure?"}
+                    onConfirm={async () => {
+                      const order = await axios.get(
+                        `${serverUrl}/sell/${data.symbol}`
+                      );
 
-            <Descriptions.Item label="Target" span={4}>
-              {data.target} ({data.targetPer}%)
-            </Descriptions.Item>
-            <Descriptions.Item label="Stoploss" span={4}>
-              {data.stopLoss} ({data.stopLossPer}%)
-            </Descriptions.Item>
-            <Descriptions.Item label="Value" span={4}>
-              x{data.quantity} = {(data.buy_price * data.quantity).toFixed(2)} &
-              Now {(data.last_price * data.quantity).toFixed(2)}
-            </Descriptions.Item>
-            {data.last_price && (
-              <Descriptions.Item label="Current Price" span={4}>
-                {data.last_price}
-              </Descriptions.Item>
+                      if (
+                        order.data &&
+                        order.data.status === "order_completed"
+                      ) {
+                        notification.success({
+                          message: "Order Success",
+                          description: "Order successfully placed.",
+                        });
+                      } else {
+                        notification.error({
+                          message: "Order Failed",
+                          description: "Order placing failed.",
+                        });
+                      }
+                    }}
+                    okText="Yes"
+                    cancelText="No"
+                    okButtonProps={{ size: "large" }}
+                    cancelButtonProps={{ size: "large" }}
+                  >
+                    <Button type="text" size="small">
+                      Sell
+                    </Button>
+                  </Popconfirm>
+                </span>
+              </Row>
+            }
+            bordered={false}
+            size="small"
+            style={{ marginBottom: 20 }}
+          >
+            {data["pnl"] > 0 ? (
+              <Progress
+                percent={+data["progress"].toFixed(1)}
+                status="active"
+                strokeColor="#73d13d"
+              />
+            ) : (
+              <Progress
+                percent={+Math.abs(data["progress"]).toFixed(1)}
+                status="active"
+                strokeColor="#ff4d4f"
+              />
             )}
-          </Descriptions>
-        </Card>
+
+            <Descriptions bordered size="small">
+              <Descriptions.Item label="Buy Date" span={4}>
+                {moment(data.buy_date).format("D MMM h:mm a")}
+              </Descriptions.Item>
+              <Descriptions.Item label="Buy Price" span={4}>
+                {data.buy_price}
+              </Descriptions.Item>
+
+              <Descriptions.Item label="Target" span={4}>
+                {data.target} ({data.targetPer}%)
+              </Descriptions.Item>
+              <Descriptions.Item label="Stoploss" span={4}>
+                {data.stopLoss} ({data.stopLossPer}%)
+              </Descriptions.Item>
+              <Descriptions.Item label="Value" span={4}>
+                x{data.quantity} = {(data.buy_price * data.quantity).toFixed(2)}{" "}
+                & Now {(data.last_price * data.quantity).toFixed(2)}
+              </Descriptions.Item>
+              {data.last_price && (
+                <Descriptions.Item label="Current Price" span={4}>
+                  {data.last_price}
+                </Descriptions.Item>
+              )}
+            </Descriptions>
+          </Card>
+        </Badge>
       </Col>
     </>
   );
