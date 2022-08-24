aa45b37f4ad74abd973912703edc4285bc751761
laxmikantphadke <laxmikantphadke@gmail.com>
2022-08-24 11:09:30 +0530

    asd

--- src/helpers/interceptor.ts
+++ src/helpers/interceptor.ts
@@ -6,10 +6,15 @@ export const LoadInterceptor = () => {
   const history = useHistory();
   axios.interceptors.request.use(
     (config) => {
-      const token = localStorage.getItem("machine_token");
+      const token = localStorage.getItem(
+        config.url?.includes("/token/refresh")
+          ? "machine_refresh_token"
+          : "machine_token"
+      );
       if (token) {
         config.headers["Authorization"] = "Bearer " + token;
       }
+
       // config.headers['Content-Type'] = 'application/json';
       return config;
     },
@@ -36,18 +41,14 @@ export const LoadInterceptor = () => {
       if (error.response.status === 401 && !originalRequest._retry) {
         originalRequest._retry = true;
         const token = localStorage.getItem("machine_refresh_token");
-        return axios
-          .get(serverUrl + "/token/refresh", {
-            headers: { Authorization: "Bearer " + token },
-          })
-          .then((res) => {
-            if (res.status === 201) {
-              localStorage.setItem("machine_token", res.data.access);
-              axios.defaults.headers.common["Authorization"] =
-                "Bearer " + localStorage.getItem("machine_token");
-              return axios(originalRequest);
-            }
-          });
+        return axios.get(serverUrl + "/token/refresh").then((res) => {
+          if (res.status === 201) {
+            localStorage.setItem("machine_token", res.data.access);
+            axios.defaults.headers.common["Authorization"] =
+              "Bearer " + localStorage.getItem("machine_token");
+            return axios(originalRequest);
+          }
+        });
       }
       return Promise.reject(error);
     }
